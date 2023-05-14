4f4e7545f0aaeda534417719c637f1cdb9f217cc
Dmambo <mambotrader0@gmail.com>
2023-05-14 23:59:21 +0530

    Fetch the Hotels and Added the delete functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Delete from './components/Delete/Delete';
-=======
 import Splash from './components/splash/Splash';
->>>>>>> 5a7c7efc7fde41513790a0c25d6e379c01858be1
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Router>
         <Navigation />
         <Routes>
+          <Route path="/" element={<Splash />} />
           <Route path="/delete-hotels" element={<Delete />} />
         </Routes>
       </Router>
-=======
-        <Splash/>
->>>>>>> 5a7c7efc7fde41513790a0c25d6e379c01858be1
     </div>
   );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,20 +5,22 @@ import './Navigation.css';
 const Navigation = () => {
   return (
     <div className="navigation">
-      <h1 className="logo">KeFi</h1>
-      <div className="navbar">
-        <NavLink exact to="/" activeClassName="active">
-          HOTELS
-        </NavLink>
-        <NavLink to="/reservation" activeClassName="active">
-          MY RESERVATION
-        </NavLink>
-        <NavLink to="/add-hotels" activeClassName="active">
-          ADD HOTELS
-        </NavLink>
-        <NavLink to="/delete-hotels" activeClassName="active">
-          DELETE HOTEL
-        </NavLink>
+      <div className="navi">
+        <h1 className="logo">KeFi</h1>
+        <div className="navbar">
+          <NavLink exact to="/hotel" activeClassName="active">
+            HOTELS
+          </NavLink>
+          <NavLink to="/reservation" activeClassName="active">
+            MY RESERVATION
+          </NavLink>
+          <NavLink to="/add-hotels" activeClassName="active">
+            ADD HOTELS
+          </NavLink>
+          <NavLink to="/delete-hotels" activeClassName="active">
+            DELETE HOTEL
+          </NavLink>
+        </div>
       </div>
     </div>
   );
