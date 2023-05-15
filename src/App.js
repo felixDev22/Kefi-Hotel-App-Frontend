9d271c63a0374afd1864b844af17133f8e6137c9
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-05-15 12:00:33 +0300

    redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navigation from './components/Navigation';
+import Main from './components/main/Main';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navigation />
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
       </Router>
     </div>
   );
