40065a547193abef14bd04ff4d6a7b27a4af9f33
felixDev22 <felixnzomo@gmail.com>
2023-05-15 11:19:39 +0300

    Add  Navlink on splash.js, add sign pic

--- src/components/splash/Splash.js
+++ src/components/splash/Splash.js
@@ -1,15 +1,29 @@
-import React from 'react'
-import Style from './splach.css'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './splach.css';
+
 export default function Splash() {
   return (
-      <div className = 'container'>
-          <div className = 'splash'>
-            <h1> Welcome to <br/> <span>KeFi</span></h1>
-          </div>
-          <div className='btns'>
-            <button className="btn login btn-primary btn-rounded mx-5 px-5">Login</button>
-            <button className="btn signup btn-primary btn-rounded mx-5 px-5">Sign Up</button>
-          </div>
+    <div className="container">
+      <div className="splash">
+        <h1>
+          Welcome to <br /> <span>KeFi</span>
+        </h1>
       </div>
-  )
+      <div className="btns">
+        <NavLink
+          to="/login"
+          className="btn login btn-primary btn-rounded mx-5 px-5"
+          activeClassName="active">
+          Login
+        </NavLink>
+        <NavLink
+          to="/signup"
+          className="btn signup btn-primary btn-rounded mx-5 px-5"
+          activeClassName="active">
+          Sign Up
+        </NavLink>
+      </div>
+    </div>
+  );
 }
