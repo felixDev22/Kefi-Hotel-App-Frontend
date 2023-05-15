dbc71b243f0de08d5ae2b259f5af36cc984ed5f2
felixDev22 <felixnzomo@gmail.com>
2023-05-15 11:18:03 +0300

    Add routes path for pages, update routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Splash from './components/splash/Splash';
+import Login from './components/login/Login';
+import './app.css';
 
 function App() {
   return (
     <div className="App">
-        <Splash/>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" component={Splash} />
+          <Route path="/login" component={Login} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
