b840217dee56ecc7fa13ace946dc329a1ef47240
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-15 23:15:14 +0000

    add logic to sign up

--- src/components/signup/SignUp.js
+++ src/components/signup/SignUp.js
@@ -1,22 +1,86 @@
-import React, { useState } from 'react';
+import React, {
+  useState,
+  useEffect
+} from 'react';
+import {
+  useDispatch,
+  useSelector
+} from 'react-redux';
+import {
+  registerUser,
+  selectRegisterData,
+  selectRegisterLoading
+} from '../../features/slices/auth/register';
+
 import '../login/login.css';
 
-export default function SignUp() {
+export default function register() {
+
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [password_confirmation, setConfirmPassword] = useState('');
+  const [errorStrings, setErrorStrings] = useState([]);
 
-  const handleEmail = (event) => {
-    setEmail(event.target.value);
+  const dispatch = useDispatch();
+  const loading = useSelector(selectRegisterLoading);
+  const registerData = useSelector(selectRegisterData);
+
+  const validateInputFields = () => {
+    const errors = [];
+    if (!name) {
+      errors.push('Name is required');
+    }
+    if (!email) {
+      errors.push('Email is required');
+    }
+    if (!password) {
+      errors.push('Password is required');
+    }
+    if (!password_confirmation) {
+      errors.push('Confirm Password is required');
+    }
+    if (password !== password_confirmation) {
+      errors.push('Password and Confirm Password must be same');
+    }
+
+    setErrorStrings(errors);
+    return errors.length === 0;
   };
 
-  const handlePassword = (event) => {
-    setPassword(event.target.value);
-  };
+  const userToken = localStorage.getItem('token');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // handle form submission
-  };
+  useEffect(() => {
+    if (userToken) {
+      // token exist so navigate user to home page
+    } else {
+      // token does not exist so navigate user to login page
+      console.log('$registerData', registerData);
+    }
+  }, [userToken, registerData]);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setErrorStrings('')
+
+    const user = {
+      name,
+      email,
+      password,
+      password_confirmation
+    };
+
+    if (validateInputFields()) {
+      dispatch(registerUser({
+        user,
+      }));
+    } else {
+      console.log('error', errorStrings);
+    }
+  }
+
 
   return (
     <div className="fluid">
@@ -31,52 +95,98 @@ export default function SignUp() {
               <h4>Welcome to the ultimate hotel booking site:</h4>
             </div>
             <form onSubmit={handleSubmit}>
+                <div className="input-filed">
+                  <input
+                    type="text"
+                    class="input"
+                    id="name"
+                    required
+                    autoComplete="off"
+                    placeholder="Full Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
               <div className="input-filed">
-                <input
-                  type="text"
-                  className="input"
-                  id="name"
-                  required
-                  autoComplete="off"
-                  placeholder="Name"
-                  value={email}
-                  onChange={handleEmail}
-                />
+                  <input
+                    type="text"
+                    class="input"
+                    id="email"
+                    required
+                    autoComplete="off"
+                    placeholder="Email Address"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
               </div>
+
+              <div>
+                <input
+                    type="text"
+                    class="input"
+                    id="password"
+                    required
+                    autoComplete="off"
+                    placeholder="Password"
+                    value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  />
+              </div>
+
               <div>
                 <input
                   type="text"
-                  className="input"
-                  id="email"
+                  class="input"
+                  id="confirm-password"
                   required
                   autoComplete="off"
-                  placeholder="Email"
-                  value={password}
-                  onChange={handlePassword}
+                  placeholder="Confirm Password"
+                  value = {
+                    password_confirmation
+                  }
+                  onChange = {
+                      (e) => setConfirmPassword(e.target.value)
+                  }
                 />
               </div>
-              <div>
-                <input
-                  type="text"
-                  className="input"
-                  id="password"
-                  required
-                  autoComplete="off"
-                  placeholder="Password"
-                  value={password}
-                  onChange={handlePassword}
-                />
-              </div>
-              <button type="submit" className="submit">
+
+              {/* <button type="submit" className="submit">
                 Sign-Up
-              </button>
+              </button> */}
+
+              {
+                  errorStrings && errorStrings.length > 0 && (
+                    <div className="error">
+                      <ul>
+                        {
+                          errorStrings.map((error) => (
+                            <li key={error}>{error}</li>
+                          ))
+                        }
+                      </ul>
+                    </div>
+                  )
+                }
+
+                {
+                  loading ? (
+                    <button type="submit" className="submit" disabled>
+                          <p>Loading...</p>
+                    </button>
+                  ) : (
+                    <button type="submit" className="submit">
+                          <p>Signup</p>
+                      </button>
+                    )
+                }
             </form>
             <div className="sign-in">
               <span
                 dangerouslySetInnerHTML={{
-                  __html: "If you have an account. <a href='./login'>Login</a>",
+                  __html: "Already have an Account?. <a href='./login'>Login</a>",
                 }}></span>
             </div>
+
           </div>
         </div>
       </div>
