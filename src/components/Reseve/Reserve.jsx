fcaec868e2ca6fa1c1eb7e768fcca992a10eeef0
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-16 14:16:40 +0000

    update the main file and create reserve files

---
+++ src/components/Reseve/Reserve.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Reserve.css'
+
+const Reserve = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Reserve
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -5,6 +5,7 @@ import hotels from '../../common/apis/hotels';
 import './Main.css';
 import Hotels from '../hotels/HotelListing';
 import { addHotel } from '../../features/hotels';
+import Navigation from '../navigation/Navigation'
 
 export default function Main() {
   const dispatch = useDispatch();
@@ -19,12 +20,15 @@ export default function Main() {
     fetchData();
   }, []);
   return (
-    <div className="container">
+    <>
+       <Navigation />
+      <div className="container">
       <hr></hr>
       <p className="title">Enjoy your stay at any of our selections</p>
       <div className="cards">
         <Hotels />
       </div>
     </div>
+    </>
   );
 }
