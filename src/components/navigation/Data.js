02d275dad83f9f44bb700c08235fa8d26c00a564
Dmambo <mambotrader0@gmail.com>
2023-05-17 00:07:23 +0530

    Fixed the navigation page

---
+++ src/components/navigation/Data.js
@@ -0,0 +1,36 @@
+export const twitterLinks = [
+  { name: 'IBRAHIM', url: 'https://twitter.com/kingibro345' },
+  { name: 'FELIX', url: 'https://twitter.com/monzo200' },
+  {
+    name: 'ELI',
+    url: 'https://www.linkedin.com/in/ely-cheikh-maaloum-075a79135/',
+  },
+  { name: 'KENNEDY', url: 'https://twitter.com/KeFi_Ke3' },
+];
+
+export const linkedinLinks = [
+  { name: 'KENNEDY', url: 'https://www.linkedin.com/company/kefi-ke/' },
+  {
+    name: 'ELI',
+    url: 'https://www.linkedin.com/in/ely-cheikh-maaloum-075a79135/',
+  },
+  { name: 'FELIX', url: 'https://www.linkedin.com/in/felixnyamai/' },
+  { name: 'IBRAHIM', url: 'https://www.linkedin.com/dmambo' },
+];
+
+export const githubLinks = [
+  { name: 'KENNEDY', url: 'https://github.com/KeFi-Ke' },
+  { name: 'ELI', url: 'https://github.com/maaloum' },
+  { name: 'FELIX', url: 'https://github.com/KeFi-Ke2' },
+  { name: 'IBRAHIM', url: 'https://github.com/dmambo' },
+];
+
+export const webLinks = [
+  { name: 'KENNEDY', url: 'https://dmambo.github.io/portfolio-web/' },
+  { name: 'ELI', url: 'https://maaloum.netlify.app/' },
+  {
+    name: 'FELIX',
+    url: 'https://deploy-preview-19--felix-portfolio-7f7ed5.netlify.app/',
+  },
+  { name: 'IBRAHIM', url: 'https://dmambo.github.io/portfolio-web/' },
+];
