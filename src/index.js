38c01eead3f3afdf0ba807e24eb4c96d55802be3
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-05-17 21:08:00 +0300

    Wrap the app inside the provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
+import store from './features/store';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
