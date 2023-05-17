a72bb0ea1f3667f2adc3d251c049861cd560f731
felixDev22 <felixnzomo@gmail.com>
2023-05-18 00:32:02 +0300

    Update addhotel, create handleNewHotel, resetForm

--- src/components/addHotel/AddHotel.js
+++ src/components/addHotel/AddHotel.js
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import { addHotel, newHotel } from '../../features/hotels';
 import '../../components/login/login.css';
 import './addHotel.css';
-import newpic from '../../Assets/sign.png';
+// import newpic from '../../Assets/sign.png';
 
 const AddHotel = () => {
+  const dispatch = useDispatch();
+  const hotels = useSelector((state) => state.hotels);
+  const [hotelName, setHotelName] = useState('');
+  const [photoURL, setPhotoURL] = useState('');
+  const [rating, setRating] = useState('');
+  const [location, setLocation] = useState('');
+  const [price, setPrice] = useState('');
+  const [desc, setDesc] = useState('');
+
+  useEffect(() => {
+    const fetchHotels = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/v1/hotels');
+        dispatch(addHotel(response.data));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchHotels();
+  }, [dispatch]);
+
+  const handleNewHotel = (e) => {
+    e.preventDefault();
+    const newHotelData = {
+      name: hotelName,
+      photo: photoURL,
+      rating: parseInt(rating),
+      location: location,
+      price: parseInt(price),
+      desc: desc,
+    };
+
+    axios
+      .post('http://localhost:3000/api/v1/hotels', newHotelData)
+      .then((response) => {
+        dispatch(newHotel(response.data));
+        resetForm();
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const resetForm = () => {
+    setHotelName('');
+    setPhotoURL('');
+    setRating('');
+    setLocation('');
+    setPrice('');
+    setDesc('');
+  };
+
   return (
     <div className="wrapper-add">
       <div className="row">
@@ -11,75 +65,79 @@ const AddHotel = () => {
           <div className="add-title">
             <h4>Add hotel:</h4>
           </div>
-          <form>
-            <div className="input-filed">
+          <form onSubmit={handleNewHotel}>
+            <div className="input-field">
               <input
                 type="text"
-                class="input"
-                id="hotel name"
+                className="input"
+                id="hotelName"
                 required
                 autoComplete="off"
-                placeholder="Hotel Name:"
-                value="Hotel Name"
+                placeholder="Hotel Name"
+                value={hotelName}
+                onChange={(e) => setHotelName(e.target.value)}
               />
             </div>
-            <div className="input-filed">
+            <div className="input-field">
               <input
                 type="text"
-                class="input"
-                id="photo"
+                className="input"
+                id="photoURL"
                 required
                 autoComplete="off"
                 placeholder="Photo URL"
-                value="Photo"
+                value={photoURL}
+                onChange={(e) => setPhotoURL(e.target.value)}
               />
             </div>
-            <div className="input-filed">
+            <div className="input-field">
               <input
-                type="text"
-                class="input"
+                type="number"
+                className="input"
                 id="rating"
                 required
                 autoComplete="off"
-                placeholder="rating"
-                value="rating"
+                placeholder="Rating"
+                value={rating}
+                onChange={(e) => setRating(e.target.value)}
               />
             </div>
-            <div className="input-filed">
+            <div className="input-field">
               <input
                 type="text"
-                class="input"
+                className="input"
                 id="location"
                 required
                 autoComplete="off"
                 placeholder="Location"
-                value="Location"
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
               />
             </div>
-            <div className="input-filed">
+            <div className="input-field">
               <input
                 type="text"
-                class="input"
+                className="input"
                 id="price"
                 required
                 autoComplete="off"
                 placeholder="Price"
-                value="Price"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
               />
             </div>
-
-            <div className="input-filed">
+            <div className="input-field">
               <input
                 type="text"
-                class="input"
+                className="input"
                 id="desc"
                 required
                 autoComplete="off"
-                placeholder="Brief Intro:"
-                value="Bio"
+                placeholder="Brief Intro"
+                value={desc}
+                onChange={(e) => setDesc(e.target.value)}
               />
             </div>
-
             <button type="submit" className="add-btn">
               <p>Add</p>
             </button>
@@ -87,13 +145,12 @@ const AddHotel = () => {
         </div>
         <div className="col-md-6 pic">
           <h4>New hotel:</h4>
-
           <div>
             <div className="new-hotel">
               <div className="new-hotel-pic">
-                <img src={newpic} alt="Hotel" />
+                <img src={photoURL} alt="Hotel" />
               </div>
-              <h5>{hotel.name}</h5>
+              <h5>{hotelName}</h5>
             </div>
           </div>
         </div>
