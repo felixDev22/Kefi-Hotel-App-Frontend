5ee1a68df216343dd2158c680aa58d175658f69b
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-18 18:53:27 +0000

    remove navigation

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -5,7 +5,6 @@ import hotels from '../../common/apis/hotels';
 import './Main.css';
 import Hotels from '../hotels/HotelListing';
 import { addHotel } from '../../features/hotels';
-import Navigation from '../navigation/Navigation'
 
 export default function Main() {
   const dispatch = useDispatch();
@@ -21,7 +20,6 @@ export default function Main() {
   }, []);
   return (
     <>
-      <Navigation />
       <div className="container">
       <hr></hr>
       <p className="title">Enjoy your stay at any of our selections</p>
