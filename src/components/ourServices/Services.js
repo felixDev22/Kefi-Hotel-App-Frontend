642152c6e98bdc01fca01d5c62dd08ea4c163129
felixDev22 <felixnzomo@gmail.com>
2023-05-19 23:26:20 +0300

    Add all the images

--- src/components/ourServices/Services.js
+++ src/components/ourServices/Services.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import conference from '../../Assets/conference.png';
 import reception from '../../Assets/reception.png';
+import spa from '../../Assets/spa.png';
+import room1 from '../../Assets/room1.png';
+import dinning from '../../Assets/dining.png';
 import play from '../../Assets/play.png';
 import './services.css';
 
@@ -18,12 +21,27 @@ export const Services = () => {
         <div className="box">
           <img src={reception} alt="pic1" className="service-pic" />
         </div>
-        <div className="box">grid 3</div>
-        <div className="box">grid 4</div>
-        <div className="box">grid 5</div>
-        <div className="box">grid 6</div>
-        <div className="box">grid 7</div>
-        <div className="box">grid 8</div>
+        <div className="box2">
+          <h6>Hospitality</h6>
+          <img src={play} alt="pic1" className="service-icon" />
+        </div>
+        <div className="box">
+          <img src={room1} alt="pic1" className="service-pic" />
+        </div>
+        <div className="box2">
+          <h6>Hospitality</h6>
+          <img src={play} alt="pic1" className="service-icon" />
+        </div>
+        <div className="box">
+          <img src={spa} alt="pic1" className="service-pic" />
+        </div>
+        <div className="box2">
+          <h6>Hospitality</h6>
+          <img src={play} alt="pic1" className="service-icon" />
+        </div>
+        <div className="box">
+          <img src={dinning} alt="pic1" className="service-pic" />
+        </div>
       </div>
     </div>
   );
