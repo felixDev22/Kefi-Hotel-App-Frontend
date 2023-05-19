08c2bebefae582fda027179e0dbaaf84d011b354
Dmambo <mambotrader0@gmail.com>
2023-05-19 22:59:02 +0530

    Merged dev branch

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import Login from './components/login/Login';
 import SignUp from './components/signup/SignUp';
 import Main from './components/main/Main';
 import Room from './components/Rooms/Room';
-import './App.css';
 import AddHotel from './components/addHotel/AddHotel';
 import Reserve from './components/Reseve/Reserve';
 import Navigation from './components/navigation/Navigation';
+import ReservedHotel from './components/Reservation/ReservedHotel';
+import './App.css';
 
 function App() {
   return (
@@ -39,7 +40,8 @@ function AppContent() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/rooms" element={<Room />} />
         <Route path="/addHotel" element={<AddHotel />} />
-        <Route path='/hotel/:id/reserve' element={<Reserve />} />
+        <Route path="/hotel/:id/reserve" element={<Reserve />} />
+        <Route path="/reserved-hotel" element={<ReservedHotel />} />
       </Routes>
     </>
   );
