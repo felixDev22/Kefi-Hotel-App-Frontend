579bcc45eec08fd080c17a9adde08d4024d77a19
felixDev22 <felixnzomo@gmail.com>
2023-05-19 21:36:56 +0300

    Create servies.js, add grid class

---
+++ src/components/ourServices/Services.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import conference from '../../Assets/conference.png';
+
+export const Services = () => {
+  return (
+    <div className="service-wrapper">
+      <div className="pic-wrapper">
+        <div>
+          <image src={conference} />
+        </div>
+        <div>grid 1</div>
+        <div>grid 1</div>
+        <div>grid 1</div>
+        <div>grid 1</div>
+        <div>grid 1</div>
+      </div>
+    </div>
+  );
+};
