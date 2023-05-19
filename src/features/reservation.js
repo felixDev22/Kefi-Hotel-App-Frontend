468fc362f638687c8b4294c4823c468a26a4c530
Dmambo <mambotrader0@gmail.com>
2023-05-19 19:32:01 +0530

    Modified the reservation

--- src/features/reservation.js
+++ src/features/reservation.js
@@ -11,8 +11,16 @@ const reservationSlice = createSlice({
     addReservation: (state, { payload }) => {
       state.reservation = payload;
     },
+    deleteReservation: (state, { payload }) => {
+      const reservationIndex = state.reservation.findIndex(
+        (reservation) => reservation.id === payload
+      );
+      if (reservationIndex !== -1) {
+        state.reservation.splice(reservationIndex, 1);
+      }
+    },
   },
 });
 
-export const { addReservation } = reservationSlice.actions;
+export const { addReservation, deleteReservation } = reservationSlice.actions;
 export default reservationSlice.reducer;
