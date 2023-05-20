7b5f8e5094ea20aea2203e8d85f06cbf7ac986ee
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-05-20 19:18:17 +0300

    remove the unecessary code

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -10,7 +10,6 @@ export default function Main() {
   const dispatch = useDispatch();
   const hotelLength = useSelector(state => state.hotels.hotels.length)
   const name = useSelector((state) => state.login.data.user.name);
-  const[ email , setEmail] = useState('');
   useEffect(() => {
     const fetchData = async () => {
       const resonse = await hotelsApi.get().catch((err) => {
@@ -18,15 +17,13 @@ export default function Main() {
       });
       dispatch(addHotel(resonse.data));
     };
-    setEmail(name)
     fetchData();
   }, []);
-  console.log(email)
   return (
     <>
         {
           hotelLength < 1 && <div className='container'> 
-             <h1>Welcome  {email}</h1> 
+             <h1>Welcome  {name}</h1> 
              <p className="text-dark"> There are no hotels yet</p>
              <a href="/add-hotel" className=" btn btn-primary">Add Hotel</a>
               
