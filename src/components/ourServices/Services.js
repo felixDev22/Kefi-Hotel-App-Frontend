e2e8d0996bfe8280629eefd2bc987f1aa1694eff
felixDev22 <felixnzomo@gmail.com>
2023-05-20 12:47:38 +0300

    Add Navlink to main page

--- src/components/ourServices/Services.js
+++ src/components/ourServices/Services.js
@@ -5,45 +5,48 @@ import spa from '../../Assets/spa.png';
 import room1 from '../../Assets/room1.png';
 import dinning from '../../Assets/dining.png';
 import play from '../../Assets/play.png';
+import { NavLink } from 'react-router-dom';
 import './services.css';
 
 export const Services = () => {
   return (
     <div className="service-wrapper">
       <div className="grid-container">
-        <div className="pic-wrapper container">
-          <div className="box">
-            <img src={conference} alt="pic1" className="service-pic" />
+        <NavLink to="/main">
+          <div className="pic-wrapper container">
+            <div className="box">
+              <img src={conference} alt="pic1" className="service-pic" />
+            </div>
+            <div className="box2">
+              <h6>Hospitality</h6>
+              <img src={play} alt="pic1" className="service-icon" />
+            </div>
+            <div className="box">
+              <img src={reception} alt="pic1" className="service-pic" />
+            </div>
+            <div className="box2">
+              <h6>Conference</h6>
+              <img src={play} alt="pic1" className="service-icon" />
+            </div>
+            <div className="box">
+              <img src={room1} alt="pic1" className="service-pic" />
+            </div>
+            <div className="box3">
+              <h6>Dining</h6>
+              <img src={play} alt="pic1" className="service-icon" />
+            </div>
+            <div className="box">
+              <img src={spa} alt="pic1" className="service-pic" />
+            </div>
+            <div className="box3">
+              <h6>Spa</h6>
+              <img src={play} alt="pic1" className="service-icon" />
+            </div>
+            <div className="box">
+              <img src={dinning} alt="pic1" className="service-pic" />
+            </div>
           </div>
-          <div className="box2">
-            <h6>Hospitality</h6>
-            <img src={play} alt="pic1" className="service-icon" />
-          </div>
-          <div className="box">
-            <img src={reception} alt="pic1" className="service-pic" />
-          </div>
-          <div className="box2">
-            <h6>Conference</h6>
-            <img src={play} alt="pic1" className="service-icon" />
-          </div>
-          <div className="box">
-            <img src={room1} alt="pic1" className="service-pic" />
-          </div>
-          <div className="box3">
-            <h6>Dining</h6>
-            <img src={play} alt="pic1" className="service-icon" />
-          </div>
-          <div className="box">
-            <img src={spa} alt="pic1" className="service-pic" />
-          </div>
-          <div className="box3">
-            <h6>Spa</h6>
-            <img src={play} alt="pic1" className="service-icon" />
-          </div>
-          <div className="box">
-            <img src={dinning} alt="pic1" className="service-pic" />
-          </div>
-        </div>
+        </NavLink>
       </div>
     </div>
   );
