7af930deb6887a59228376f0a08554f4d0fdf68a
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-05-20 14:51:33 +0300

    add the reduce

--- src/features/store.js
+++ src/features/store.js
@@ -1,4 +1,4 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore} from '@reduxjs/toolkit';
 import hotelsReducer from './hotels';
 import roomsReducer from './room';
 import logoutReducer from '../features/slices/auth/logout';
