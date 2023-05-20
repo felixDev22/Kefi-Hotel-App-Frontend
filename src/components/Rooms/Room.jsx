380ceb492a1fbb1bfc6ab7b26a78486617e2e32e
Dmambo <mambotrader0@gmail.com>
2023-05-20 20:07:57 +0530

    Refactor of code

--- src/components/Rooms/Room.jsx
+++ src/components/Rooms/Room.jsx
@@ -1,38 +1,89 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchRooms, selectRooms } from '../../features/room';
+import { addRooms } from '../../features/room';
+import { useParams } from 'react-router-dom';
+import rectangle from '../../Assets/rectangle.png';
 import axios from 'axios';
+import './Room.css';
 
-const Room = ({ hotelId, roomTypeId }) => {
+const Room = () => {
   const dispatch = useDispatch();
-  const rooms = useSelector(selectRooms);
-  console.log(rooms);
+  const rooms = useSelector((state) => state.rooms.rooms);
+
+  console.log('rooms:', rooms);
+
+  const { hotel_id, room_type_id } = useParams();
+  console.log(hotel_id, room_type_id);
+
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
-    const fetchRoomsData = async () => {
+    dispatch(addRooms([]));
+
+    const fetchRooms = async () => {
       try {
         const response = await axios.get(
-          `http://localhost:3000/api/v1/hotels/${hotelId}/room_types/${roomTypeId}/rooms`
+          `http://localhost:3000/api/v1/hotels/${hotel_id}/room_types/${room_type_id}/rooms`
         );
-        dispatch(fetchRooms(response.data));
+        console.log(response.data);
+        dispatch(addRooms(response.data));
       } catch (error) {
         console.error(error);
       }
     };
-    fetchRoomsData();
-  }, [dispatch, hotelId, roomTypeId]);
+    fetchRooms();
+  }, []);
+
+  useEffect(() => {
+    const slideInterval = setInterval(() => {
+      setCurrentSlide((prevSlide) =>
+        prevSlide === rooms.length - 1 ? 0 : prevSlide + 1
+      );
+    }, 3000);
+
+    return () => {
+      clearInterval(slideInterval);
+    };
+  }, [rooms.length]);
+
+  const handleSlideChange = (slideIndex) => {
+    setCurrentSlide(slideIndex);
+  };
 
   return (
-    <div className="rooms">
-      <h1>Rooms</h1>
-      {rooms.map((room) => (
-        <div className="room" key={room.id}>
-          <h2>{room.room_name}</h2>
-          <h3>{room.price}</h3>
-          <h3>{room.desc}</h3>
-          <img src={room.photo} alt="Room" />
-        </div>
-      ))}
+    <div className="roomsz">
+      <div className="info">
+        <img src={rectangle} alt="rectangle" />
+        <p>Get a Glimpse of the Rooms</p>
+      </div>
+      <div className="slideshow-container">
+        {rooms.map((room, index) => {
+          const slideClass =
+            index === currentSlide
+              ? 'room active'
+              : index === currentSlide - 1 ||
+                (currentSlide === 0 && index === rooms.length - 1)
+              ? 'room previous'
+              : 'room next';
+
+          return (
+            <div className={slideClass} key={room.id}>
+              <div className="image">
+                <img src={room.photo} alt="Room" />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="dots-container">
+        {rooms.map((_, index) => (
+          <span
+            key={index}
+            className={`dot ${index === currentSlide ? 'active' : ''}`}
+            onClick={() => handleSlideChange(index)}
+          ></span>
+        ))}
+      </div>
     </div>
   );
 };
