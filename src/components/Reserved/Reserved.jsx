8f722c26bc266cd3a7b83eb931e57ba1bb97d330
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-05-24 17:06:28 +0300

    Fix linters for Reserved Hotel page

--- src/components/Reserved/Reserved.jsx
+++ src/components/Reserved/Reserved.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ReservedItem from '../ReservedItem/ReservedItem';
 import rightarr from '../../Assets/rightarr.png';
 import leftarr from '../../Assets/leftarr.png';
 
-const Reserved = ({ reservation, currentIndex, onNext, onPrev, onDelete }) => {
+function Reserved({
+  reservation, currentIndex, onNext, onPrev, onDelete,
+}) {
   const numVisibleHotels = window.innerWidth < 768 ? 1 : 3;
   return (
     <div className="delete">
@@ -14,8 +17,8 @@ const Reserved = ({ reservation, currentIndex, onNext, onPrev, onDelete }) => {
         onClick={onPrev}
       />
       <div className="hotelListss">
-        {reservation.length > 0 &&
-          reservation
+        {reservation.length > 0
+          && reservation
             .slice(currentIndex, currentIndex + numVisibleHotels)
             .map((reservation) => (
               <ReservedItem
@@ -33,6 +36,27 @@ const Reserved = ({ reservation, currentIndex, onNext, onPrev, onDelete }) => {
       />
     </div>
   );
+}
+
+Reserved.propTypes = {
+  reservation: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      photo: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      totalPrice: PropTypes.number.isRequired,
+      room_type: PropTypes.string.isRequired,
+      check_in: PropTypes.string.isRequired,
+      check_out: PropTypes.string.isRequired,
+      adults: PropTypes.number.isRequired,
+      children: PropTypes.number.isRequired,
+      rooms: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+  currentIndex: PropTypes.number.isRequired,
+  onNext: PropTypes.func.isRequired,
+  onPrev: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default Reserved;
