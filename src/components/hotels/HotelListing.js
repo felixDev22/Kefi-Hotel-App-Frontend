bbe346b6dd02ab093d81c407f94f162079396bbb
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-05-24 17:02:51 +0300

    Fix linters for Hotel Item page

--- src/components/hotels/HotelListing.js
+++ src/components/hotels/HotelListing.js
@@ -1,6 +1,7 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react';
-import Hotel from '../hotel/hotel';
 import { useSelector } from 'react-redux';
+import Hotel from '../hotel/hotel';
 import rightarr from '../../Assets/rightarr.png';
 import leftarr from '../../Assets/leftarr.png';
 import './hotellisting.css';
@@ -8,6 +9,7 @@ import '../Delete/Delete.css';
 
 export default function HotelListing() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const hotels = useSelector((state) => state.hotels.hotels);
   const handleNext = () => {
     setCurrentIndex((currentIndex + 1) % hotels.length);
   };
@@ -15,36 +17,28 @@ export default function HotelListing() {
   const handlePrev = () => {
     setCurrentIndex(currentIndex === 0 ? hotels.length - 1 : currentIndex - 1);
   };
-
-  const hotels = useSelector((state) => state.hotels.hotels);
-
-  // Determine the number of visible hotels based on the viewport width
   const numVisibleHotels = window.innerWidth < 768 ? 1 : 3;
 
   return (
-    <>
-      <div className="deleted">
-        <img
-          src={leftarr}
-          className="leftarr"
-          alt="left arrow"
-          onClick={handlePrev}
-        />
-        <div className="hotelList">
-          {hotels.length > 0 &&
-            hotels
+    <div className="deleted">
+      <img
+        src={leftarr}
+        className="leftarr"
+        alt="left arrow"
+        onClick={handlePrev}
+      />
+      <div className="hotelList">
+        {hotels.length > 0
+            && hotels
               .slice(currentIndex, currentIndex + numVisibleHotels)
-              .map((hotel) => {
-                return <Hotel hotel={hotel} key={hotel.id} />;
-              })}
-        </div>
-        <img
-          src={rightarr}
-          className="rightarr"
-          alt="right arrow"
-          onClick={handleNext}
-        />
+              .map((hotel) => <Hotel hotel={hotel} key={hotel.id} />)}
       </div>
-    </>
+      <img
+        src={rightarr}
+        className="rightarr"
+        alt="right arrow"
+        onClick={handleNext}
+      />
+    </div>
   );
 }
