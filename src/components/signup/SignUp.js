0e5992e26e353d55059df2571ee8473ac4d31d63
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-05-24 17:10:00 +0300

    Fix linters for SignUp page

--- src/components/signup/SignUp.js
+++ src/components/signup/SignUp.js
@@ -1,22 +1,22 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { registerUser } from '../../features/slices/auth/register';
 import { RiEyeFill, RiEyeOffFill } from 'react-icons/ri';
 import { Navigate } from 'react-router-dom';
 import '../login/login.css';
 import axios from 'axios';
+import { registerUser } from '../../features/slices/auth/register';
 import logo from '../../Assets/KefI-logo-white.png';
 
 export default function SignUp() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [password_confirmation, setConfirmPassword] = useState('');
+  const [passwordConfirmation, setConfirmPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
   const dispatch = useDispatch();
   const errorStrings = useSelector((state) => state.register.errors);
   const iscreated = useSelector((state) => state.register.iscreated);
-  console.log(iscreated);
   const handleTogglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
@@ -27,52 +27,59 @@ export default function SignUp() {
         'http://127.0.0.1:3000/signup',
         {
           user: {
-            name :name,
-            email: email,
-            password: password,
-            password_confirmation: password_confirmation,
+            name,
+            email,
+            password,
+            passwordConfirmation,
           },
         },
         { withCredentials: true },
       )
       .then((response) => {
         if (response.data.status === 'created') {
+          // eslint-disable-next-line no-console
           console.log('Registration data', response.data);
         }
       })
       .catch((error) => {
+        // eslint-disable-next-line no-console
         console.log('registration error', error);
       });
-      dispatch(
-        registerUser({
-          user: {
-            name: name,
-            email: email,
-            password: password,
-          },
-        }),
-      );
-
+    dispatch(
+      registerUser({
+        user: {
+          name,
+          email,
+          password,
+        },
+      }),
+    );
   };
   return (
     <div className="fluid">
       <div className="row">
         <div className="col-md-6 side-image">
-          <img src={logo} className="klogo" alt="logo"></img>
+          <img src={logo} className="klogo" alt="logo" />
         </div>
         <div className="col-md-6 right">
           <div className="input-box">
             <div className="intro">
-              <span className="line"></span>
+              <span className="line" />
               <h4>Welcome to the ultimate hotel booking site:</h4>
             </div>
-            {errorStrings && <p> {errorStrings} </p>}
+            {errorStrings && (
+            <p>
+              {' '}
+              {errorStrings}
+              {' '}
+            </p>
+            )}
             {iscreated && <Navigate to="/login" />}
             <form onSubmit={handleSubmit}>
               <div className="input-filed">
                 <input
                   type="text"
-                  class="input"
+                  className="input"
                   id="name"
                   required
                   autoComplete="off"
@@ -84,7 +91,7 @@ export default function SignUp() {
               <div className="input-filed">
                 <input
                   type="text"
-                  class="input"
+                  className="input"
                   id="email"
                   required
                   autoComplete="off"
@@ -107,41 +114,47 @@ export default function SignUp() {
                   />
                   <span
                     className="see-password"
-                    onClick={handleTogglePasswordVisibility}>
+                    onClick={handleTogglePasswordVisibility}
+                  >
+                    {showPassword ? <RiEyeOffFill /> : <RiEyeFill />}
+                  </span>
+
+                </div>
+
+              </div>
+              <div className="input-filed">
+                <div className="input-wrapper">
+                  <input
+                    type={showPassword ? 'text' : 'password'}
+                    className="input"
+                    id="confirm-password"
+                    required
+                    autoComplete="off"
+                    placeholder="Confirm Password"
+                    value={passwordConfirmation}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
+                  <span
+                    className="see-password"
+                    onClick={handleTogglePasswordVisibility}
+                  >
                     {showPassword ? <RiEyeOffFill /> : <RiEyeFill />}
                   </span>
                 </div>
               </div>
-              <div className="input-filed">
-              <div className="input-wrapper">
-              <input
-                type={showPassword ? 'text' : 'password'}
-                  class="input"
-                  id="confirm-password"
-                  required
-                  autoComplete="off"
-                  placeholder="Confirm Password"
-                  value={password_confirmation}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-                  <span
-                    className="see-password"
-                    onClick={handleTogglePasswordVisibility}>
-                  {showPassword ? <RiEyeOffFill /> : <RiEyeFill />}
-                </span>
-              </div>
-              </div>
               <button type="submit" className="submit">
                 <p>Signup</p>
               </button>
-              {iscreated && <Navigate to="/login" replace={true} />}
+              {iscreated && <Navigate to="/login" replace />}
             </form>
             <div className="sign-in">
               <span
+                // eslint-disable-next-line react/no-danger
                 dangerouslySetInnerHTML={{
                   __html:
                     "Already have an Account?. <a href='./login'>Login</a>",
-                }}></span>
+                }}
+              />
             </div>
           </div>
         </div>
